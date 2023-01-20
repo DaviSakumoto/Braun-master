717806fecf6551f3f29b071d56001cc48c4807c3
davi <dsakumot@gmail.com>
2023-01-20 18:28:19 -0300

    up

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".slide-container", {
   slidesPerView: 3,
   spaceBetween: 30,
   slidesPerGroup: 3,
   loop: true,
-  loopFillGroupWithBlank: true,
-  /* pagination: {
+ centerSlide: "true", 
+ grabCursor: "true",
+ fade: "true", 
+  pagination: {
     el: ".swiper-pagination",
     clickable: true,
-  }, */
+    dynamicBullets: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
+  breakpoints: {
+    0:{
+      slidesPerView: 1,
+    },
+    520:{
+      slidesPerView: 1,
+    },
+    768:{
+      slidesPerView:3,
+    },
+    1000:{
+      slidesPerView: 3,
+    }
+
+  }
 });
